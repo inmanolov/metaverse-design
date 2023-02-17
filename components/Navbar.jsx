1baf7eb33f76644c7d6fa0efef3484884f38139a
unknown <ivan.nadkov@gmail.com>
2023-02-17 20:07:58 +0200

    Feedback section started

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,6 @@ import { motion } from "framer-motion";
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
 
-
 const Navbar = () => (
   <motion.nav
     variants={navVariants}
