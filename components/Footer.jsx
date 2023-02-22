5b2a1005ffa0e523a44f38bad66bde808881ec40
unknown <ivan.nadkov@gmail.com>
2023-02-22 19:53:09 +0200

    Footer started

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,19 @@
 'use client';
+import { motion } from 'framer-motion';
+import { socials } from '../constants';
+
+import styles from '../styles';
+import { footerVariants } from '../utils/motion';
 
 const Footer = () => (
-  <footer>
-    footer
-  </footer>
+  <motion.footer
+    variants={footerVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    
+  </motion.footer>
 );
 
 export default Footer;
